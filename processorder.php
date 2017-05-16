d0b14ee3e4ca04fc1994d6ba23286e1b0acb5dfb
yy523697597 <523697597@qq.com>
2017-05-16 17:09:04 +0800

    V1.0 设定默认时区为中国

--- processorder.php
+++ processorder.php
@@ -5,12 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>form1表单处理</title>
+    <style>
+        body{
+            font-family: "微软雅黑";
+            width:500px;
+            margin:0 auto;
+        }
+    </style>
 </head>
 <body>
     <h1>汽车配件</h1>
     <h2>订单详情</h2>
     <?php 
         echo '<p>订单处理完毕,时间是: ';
+//        设定默认时区
+        date_default_timezone_set('PRC');
         echo date('H:i,JS F Y');
         echo '</p>';
         $tireqty = $_POST['tireqty'];
@@ -19,9 +28,9 @@
 
         echo '<p>你的订单列表如下:';
         echo '<br>';
-        echo $tireqty.'tires<br>';
-        echo $oilqty.'bottle of oil<br>';
-        echo $sparkqty.'spark plugs<br>';
+        echo $tireqty.' tires<br>';
+        echo $oilqty.' bottle of oil<br>';
+        echo $sparkqty.' spark plugs<br>';
         echo '</p>';
     ?>
 </body>
